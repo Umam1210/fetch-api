37906b9741fbd221bbb79ed414741247edbbb0a8
khairul umam <khairulumam950@gmail.com>
2022-10-18 20:32:11 +0700

    test 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const getProduct = async () => {
+    const response = await fetch("https://dummyjson.com/products");
+    const restJson = await response.json();
+    console.log({ restJson: restJson.products });
+    setProducts(restJson.products);
+  };
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row xs={1} md={4} className="g-4">
+        {products.map((item) => {
+          return (
+            <Col>
+              <Card style={{ width: "18rem" }} className="my-4 mx-3">
+                <Card.Img variant="top" src={item?.thumbnail} width ="200px"/>
+                <Card.Body>
+                  <Card.Title>{item?.title}</Card.Title>
+                  <Card.Text>{item?.description}</Card.Text>
+                </Card.Body>
+                <Card.Title>price : {item?.price}</Card.Title>
+              <p>stock : {item?.stock}</p>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
     </div>
   );
 }
